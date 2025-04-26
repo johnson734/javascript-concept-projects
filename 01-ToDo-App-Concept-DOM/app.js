2850da5f285cab83f09ad5e6f52af0c629684d3d
Johnson <johnsonsamuvel2002@gmail.com>
2025-04-27 00:50:24 +0530

    🧠 Learned JS DOM: created 📝 todo list with ➕ add button and dynamic elements

---
+++ 01-ToDo-App-Concept-DOM/app.js
@@ -0,0 +1,37 @@
+//selectors
+
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click',addTodo);
+
+
+
+//Functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv =document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo=document.createElement('li');
+    newTodo.innerHTML='hey';
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton=document.createElement("button");
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton)
+    //TRASH BUTTON
+    const trashButton=document.createElement("button");
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+}
+
